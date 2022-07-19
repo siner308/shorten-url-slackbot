7ab2f2df96bf7fb1d1df03cfaebec969d892934c
siner308 <siner308@gmail.com>
2022-07-20 03:19:10 +0900

    fix to autofield

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Shortener(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    id = models.AutoField(primary_key=True)
     url = models.CharField(max_length=2048, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, auto_created=True)
 
